e9f8f4952b5fed95fac50a2a796d5f0811d4f602
jopyslaw <jopyslaw@gmail.com>
2025-03-02 18:42:34 +0100

    CSRF Example

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import change_email_non_protected, change_email_protected, csrf_attack_non_protected, csrf_attack_protected
 
 urlpatterns = [
+    path("change-email-non-protected/", change_email_non_protected),
+    path('change-email-protected/', change_email_protected),
+    path("csrf-attack-non-protected/", csrf_attack_non_protected),
+    path('csrf-attack-protected/', csrf_attack_protected)
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt  # Wyłączamy ochronę CSRF
 
 # Create your views here.
+@login_required
+@csrf_exempt  
+def change_email_non_protected(request):
+    if request.method == "POST":
+        new_email = request.POST.get("email")
+        request.user.email = new_email
+        request.user.save()
+        return HttpResponse(f"Email zmieniony na: {new_email}")
+    return render(request, "change_mail_non_protected.html")
+
+@login_required
+def change_email_protected(request):
+    if request.method == "POST":
+        new_email = request.POST.get("email")
+        request.user.email = new_email
+        request.user.save()
+        return HttpResponse(f"Email zmieniony na: {new_email}")
+    return render(request, "change_mail_protected.html")
+
+@login_required
+def csrf_attack_non_protected(request):
+    return render(request, "csrf_attack_non_protected.html")
+
+@login_required
+def csrf_attack_protected(request):
+    return render(request, "csrf_attack_protected.html")
