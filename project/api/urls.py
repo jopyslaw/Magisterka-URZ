7992fe0151043f2a7a4467b29560f2ec1d43fb80
jopyslaw <jopyslaw@gmail.com>
2025-03-02 15:03:20 +0100

    XSS Example

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import comment_view_unsecured, comment_view_secured
 
 urlpatterns = [
+    path('comment-unsecured/',comment_view_unsecured),
+    path('comment-secured/', comment_view_secured)
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def comment_view_unsecured(request):
+    comment = request.GET.get("comment", "")
+    return render(request, "comment-unsecured.html", {"comment": comment})
+
+def comment_view_secured(request):
+    comment = request.GET.get("comment", "")
+    return render(request, "comment-secured.html", {"comment": comment})
