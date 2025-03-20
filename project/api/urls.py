e6fb68ba438ddd8452023e964aed30282fb97b39
jopyslaw <jopyslaw@gmail.com>
2025-03-20 16:16:29 +0100

    Login rate limit example

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import vulnerable_login, secured_login
 
 urlpatterns = [
+    path('insecured-login/',vulnerable_login),
+    path('secured-login/', secured_login)
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django_ratelimit.decorators import ratelimit
 
 # Create your views here.
+@csrf_exempt
+def vulnerable_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Zalogowano pomyślnie!")
+        else:
+            return HttpResponse("Nieprawidłowe dane logowania.")
+    return render(request, "login.html")
+
+@csrf_exempt
+@ratelimit(key='ip', rate='3/m', block=True)
+def secured_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Zalogowano pomyślnie!")
+        else:
+            return HttpResponse("Nieprawidłowe dane logowania.")
+    return HttpResponse("Strona logowania")
