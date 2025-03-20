cc0538ce84aae78d10b3ed966425563b30b031a4
jopyslaw <jopyslaw@gmail.com>
2025-03-20 13:22:22 +0100

    Open redirect example

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import safe_redirect, unsafe_redirect
 
 urlpatterns = [
+    path("redirect-non-protected/", unsafe_redirect),
+    path("redirect-protected/", safe_redirect)
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import url_has_allowed_host_and_scheme
+
 
 # Create your views here.
+def unsafe_redirect(request):
+    if request.method == "GET":
+        next_url = request.GET.get('next', '/')
+        return redirect(next_url)
+
+
+def safe_redirect(request):
+    if request.method == "GET":
+        next_url = request.GET.get('next', '/')
+        allowed_hosts = {request.get_host()}
+        if url_has_allowed_host_and_scheme(next_url, allowed_hosts=allowed_hosts):
+            return redirect(next_url)
+        return redirect('/')
+    
+
+
+    
