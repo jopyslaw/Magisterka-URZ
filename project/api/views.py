9f65cf01c1eaebb07e6d4292027e51d3aeed52e4
jopyslaw <jopyslaw@gmail.com>
2025-03-30 16:18:36 +0200

    Example of cookie no expire

--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            response = HttpResponse(f"Zalogowano jako {user.username}")
+            return response
+        else:
+            return HttpResponse("Błędne dane logowania")
+    
+    return render(request, 'login.html')
