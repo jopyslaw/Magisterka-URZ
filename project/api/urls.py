710d25558a7e0935e1fe8934866274abe20878c6
jopyslaw <jopyslaw@gmail.com>
2025-04-28 12:07:25 +0200

    Clickjacking example

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import change_notifications
 
 urlpatterns = [
+    path('change-notifications/', change_notifications),
+
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-# Create your views here.
+
+@csrf_exempt
+def change_notifications(request):
+    if request.method == "POST":
+        notification_status = request.POST.get('notifications', 'off')
+        return HttpResponse(f"<h1>Powiadomienia zostały {'włączone' if notification_status == 'on' else 'wyłączone'}!</h1>")
+    
+    return HttpResponse('''
+        <html>
+            <head>
+                <title>Zmiana ustawienia powiadomień</title>
+            </head>
+            <body>
+                <form method="POST" action="/api/change-notifications/">
+                    <button type="submit" name="notifications" value="off" style="width:300px; height:100px;">Wyłącz powiadomienia</button>
+                    <button type="submit" name="notifications" value="on" style="width:300px; height:100px;">Włącz powiadomienia</button>
+                </form>
+            </body>
+        </html>
+    ''')
