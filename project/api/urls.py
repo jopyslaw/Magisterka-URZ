2ec318eb5f97f7cebba22a67d98bcc14cd482977
jopyslaw <jopyslaw@gmail.com>
2025-04-30 14:48:42 +0200

    RCE exmaple

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import unsafe_deserialize, safe_deserialize
 
 urlpatterns = [
+    path('unsafe-deserialize/', unsafe_deserialize),
+    path('safe-deserialize/', safe_deserialize)
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import pickle
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt 
+def unsafe_deserialize(request):
+    if request.method == 'POST':
+        try:
+            encoded_data = request.body
+            deserialized_data = pickle.loads(encoded_data)
+            
+            return HttpResponse("Dane zdeserializowane!")
+        except Exception as e:
+            print(e)
+            return HttpResponse(f"Błąd: {str(e)}", status=500)
+    return HttpResponse("Wyślij dane POST")
+
+@csrf_exempt
+def safe_deserialize(request):
+    if request.method == 'POST':
+        json_data = request.body.decode('utf-8')  
+
+        try:
+            deserialized_data = json.loads(json_data)
+            return HttpResponse("Dane bezpiecznie zdeserializowane")
+        except json.JSONDecodeError:
+            return HttpResponse("Nieprawidłowe dane JSON", status=400)
